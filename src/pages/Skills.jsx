4210498e9985d4fbeb023d453cfb25ee3844a4a4
Adi-1331 <sharmaadit098@gmail.com>
2026-03-05 17:06:55 +0530

    fixed svg issues

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -9,46 +9,46 @@ const Skills = () => {
       <div className='flex flex-wrap gap-4 items-center justify-evenly text-white mt-4 text-center'>
         <div>
           <div>
-            <img className='size-23' src="assets/html.svg" alt="HTML" />
+            <img className='size-23' src="/assets/html.svg" alt="HTML" />
             <p>HTML</p>
           </div>
           <div>
-            <img className='size-23' src="assets/css.svg" alt="CSS" />
+            <img className='size-23' src="/assets/css.svg" alt="CSS" />
             <p>CSS</p>
           </div>
         </div>
          <div>
           <div>
-            <img className='size-23' src="assets/javascript.svg" alt="JavaScript" />
+            <img className='size-23' src="/assets/javascript.svg" alt="JavaScript" />
             <p>JavaScript</p>
           </div>
           <div>
-            <img className='size-23 invert' src="assets/express.svg" alt="Express.js" />
+            <img className='size-23 invert' src="/assets/express.svg" alt="Express.js" />
             <p>Express.js</p>
           </div>
           </div>
           <div>
           <div>
-            <img className='size-23' src="assets/reactjs.svg" alt="React.js" />
+            <img className='size-23' src="/assets/reactjs.svg" alt="React.js" />
             <p>React.js</p>
           </div>
           <div>
-            <img className='size-23' src="assets/nodejs.svg" alt="Node.js" />
+            <img className='size-23' src="/assets/nodejs.svg" alt="Node.js" />
             <p>Node.js</p>
           </div>
         </div>
         <div>
           <div>
-            <img className='size-23' src="assets/github.svg" alt="Github" />
+            <img className='size-23' src="/assets/github.svg" alt="Github" />
             <p>Github</p>
           </div>
            <div>
-            <img className='size-23' src="assets/tailwind.svg" alt="Tailwind CSS" />
+            <img className='size-23' src="/assets/tailwind.svg" alt="Tailwind CSS" />
             <p>Tailwind CSS</p>
           </div>
         </div>
         <div className="flex flex-col items-center flex-wrap justify-center gap-1">
-          <img className='size-80' src="assets/skills.png" alt="Skill-Img" />
+          <img className='size-80' src="/assets/skills.png" alt="Skill-Img" />
             <p className='font-mono text-white'>"Coding isn’t just about writing<br/> lines of code — it’s about solving<br/> problems, creating experiences,<br/> and bringing ideas to life."</p>
           <div>____</div>
         </div>
